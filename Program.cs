27061d2e1bcd58bda9348932b4d96a193ff9fd24
Andres Sainz <andresmsainz@gmail.com>
2021-05-10 23:20:59 -0400

    initial commit: git commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Structs!");
+
+            Employee newEmployee = new Employee();
+            newEmployee.employeeName = "James";
+            newEmployee.employeeJob = "Programmer";
+            newEmployee.Salary = 5000;
+
+            Console.WriteLine($"New Employee name = {newEmployee.employeeName} and his title is {newEmployee.employeeJob} with a salary of {newEmployee.Salary}");
+        }
+    }
+
+    struct Employee
+    {
+        public string employeeName;
+        public string employeeJob;
+        private decimal salary;
+
+        public decimal Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+    }
+}
